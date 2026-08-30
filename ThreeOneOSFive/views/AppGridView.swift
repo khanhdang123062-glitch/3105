e50a947a01e47c94794ef2292c2d378be0dccbdf
khanhdang123062-glitch <khanhdang123062@gmail.com>
2026-08-30 08:30:02 +0700

    Update AppGridView.swift

--- ThreeOneOSFive/views/AppGridView.swift
+++ ThreeOneOSFive/views/AppGridView.swift
@@ -10,6 +10,10 @@ private struct HardcodedApp {
 private let targetApps: [HardcodedApp] = [
     HardcodedApp(bundleID: "com.garena.game.kgvn", displayName: "Liên Quân Mobile"),
     HardcodedApp(bundleID: "com.dts.freefireth", displayName: "Free Fire"),
+    HardcodedApp(bundleID: "com.dts.freefiremax", displayName: "Free Fire Max"),
+    HardcodedApp(bundleID: "vn.vng.pubgmobile", displayName: "PUBG"),
+    HardcodedApp(bundleID: "com.lemon.lvoverseas", displayName: "Capcut"),
+    HardcodedApp(bundleID: "Dazz.camera.vintagecamera", displayName: "Dazz cam"),
 ]
 
 struct AppGridView: View {
