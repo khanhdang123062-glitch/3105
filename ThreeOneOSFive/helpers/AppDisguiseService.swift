d501bdacf3f9b256ea552e771a4c3e8f230b69f3
khanhdang123062-glitch <khanhdang123062@gmail.com>
2026-08-30 23:41:16 +0700

    Update AppDisguiseService.swift

--- ThreeOneOSFive/helpers/AppDisguiseService.swift
+++ ThreeOneOSFive/helpers/AppDisguiseService.swift
@@ -2,123 +2,125 @@ import Foundation
 import UIKit
 
 enum AppDisguiseError: LocalizedError {
-    case bundlePathNotFound
-    case infoPlistNotFound
+    case bundleNotFound(String)
+    case plistNotFound
     case writeFailed
-    case iconWriteFailed
 
     var errorDescription: String? {
         switch self {
-        case .bundlePathNotFound: return "Không tìm thấy bundle path của app."
-        case .infoPlistNotFound: return "Không tìm thấy Info.plist."
-        case .writeFailed: return "Ghi Info.plist thất bại. Exploit chưa active?"
-        case .iconWriteFailed: return "Ghi icon thất bại."
+        case .bundleNotFound(let id):
+            return "Không tìm thấy bundle của \(id). Exploit chưa active?"
+        case .plistNotFound:
+            return "Không tìm thấy Info.plist của app."
+        case .writeFailed:
+            return "Ghi Info.plist thất bại."
         }
     }
 }
 
 struct AppDisguiseConfig: Codable {
     var displayName: String
-    var iconName: String // tên icon preset
+    var iconName: String
 }
 
 enum AppDisguiseService {
-    private static let configKey = "app.disguise.config"
+    private static let configPrefix = "app.disguise."
 
-    static var currentConfig: AppDisguiseConfig? {
-        guard let data = UserDefaults.standard.data(forKey: configKey),
+    static func currentConfig(bundleID: String) -> AppDisguiseConfig? {
+        guard let data = UserDefaults.standard.data(forKey: configPrefix + bundleID),
               let config = try? JSONDecoder().decode(AppDisguiseConfig.self, from: data)
         else { return nil }
         return config
     }
 
-    static func apply(displayName: String, iconName: String, iconImage: UIImage) throws {
-        let bundlePath = Bundle.main.bundlePath
-        let plistPath = (bundlePath as NSString).appendingPathComponent("Info.plist")
+    static func apply(bundleID: String, displayName: String, iconName: String, iconImage: UIImage) throws {
+        let plistURL = try findPlist(bundleID: bundleID)
 
-        guard FileManager.default.fileExists(atPath: plistPath) else {
-            throw AppDisguiseError.infoPlistNotFound
+        guard var plist = NSDictionary(contentsOf: plistURL) as? [String: Any] else {
+            throw AppDisguiseError.plistNotFound
         }
 
-        guard var plist = NSDictionary(contentsOfFile: plistPath) as? [String: Any] else {
-            throw AppDisguiseError.infoPlistNotFound
-        }
-
-        // Đổi display name
         plist["CFBundleDisplayName"] = displayName
         plist["CFBundleName"] = displayName
 
-        // Ghi lại Info.plist
-        let plistData = try PropertyListSerialization.data(
-            fromPropertyList: plist,
-            format: .xml,
-            options: 0
-        )
+        let data = try PropertyListSerialization.data(fromPropertyList: plist, format: .xml, options: 0)
         do {
-            try plistData.write(to: URL(fileURLWithPath: plistPath))
+            try data.write(to: plistURL)
         } catch {
             throw AppDisguiseError.writeFailed
         }
 
-        // Ghi icon mới vào bundle
-        if let iconData = iconImage.pngData() {
-            let iconSizes = [
-                ("AppIcon60x60@2x.png", 120),
-                ("AppIcon60x60@3x.png", 180),
-                ("AppIcon76x76@2x~ipad.png", 152),
-            ]
-            for (filename, size) in iconSizes {
-                let iconPath = (bundlePath as NSString).appendingPathComponent(filename)
-                let resized = resize(image: iconImage, to: CGSize(width: size, height: size))
-                if let data = resized?.pngData() {
-                    try? data.write(to: URL(fileURLWithPath: iconPath))
-                }
+        // Ghi icon vào bundle
+        let bundleDir = plistURL.deletingLastPathComponent()
+        let iconSizes: [(String, Int)] = [
+            ("AppIcon60x60@2x.png", 120),
+            ("AppIcon60x60@3x.png", 180),
+            ("AppIcon76x76@2x~ipad.png", 152),
+        ]
+        for (filename, size) in iconSizes {
+            let iconURL = bundleDir.appendingPathComponent(filename)
+            if let resized = resize(image: iconImage, to: CGSize(width: size, height: size)),
+               let imgData = resized.pngData() {
+                try? imgData.write(to: iconURL)
             }
         }
 
         // Lưu config
         let config = AppDisguiseConfig(displayName: displayName, iconName: iconName)
-        if let data = try? JSONEncoder().encode(config) {
-            UserDefaults.standard.set(data, forKey: configKey)
+        if let configData = try? JSONEncoder().encode(config) {
+            UserDefaults.standard.set(configData, forKey: configPrefix + bundleID)
         }
 
-        // Notify SpringBoard refresh
         notifySpringBoard()
     }
 
-    static func reset() throws {
-        let bundlePath = Bundle.main.bundlePath
-        let plistPath = (bundlePath as NSString).appendingPathComponent("Info.plist")
+    static func reset(bundleID: String) throws {
+        let plistURL = try findPlist(bundleID: bundleID)
 
-        guard var plist = NSDictionary(contentsOfFile: plistPath) as? [String: Any] else {
-            throw AppDisguiseError.infoPlistNotFound
+        guard var plist = NSDictionary(contentsOf: plistURL) as? [String: Any] else {
+            throw AppDisguiseError.plistNotFound
         }
 
-        plist["CFBundleDisplayName"] = "3105"
-        plist["CFBundleName"] = "3105"
+        // Restore original name from bundle ID last component
+        let originalName = plist["CFBundleName"] as? String ?? bundleID
+        plist["CFBundleDisplayName"] = originalName
 
-        let plistData = try PropertyListSerialization.data(
-            fromPropertyList: plist,
-            format: .xml,
-            options: 0
-        )
+        let data = try PropertyListSerialization.data(fromPropertyList: plist, format: .xml, options: 0)
         do {
-            try plistData.write(to: URL(fileURLWithPath: plistPath))
+            try data.write(to: plistURL)
         } catch {
             throw AppDisguiseError.writeFailed
         }
 
-        UserDefaults.standard.removeObject(forKey: configKey)
+        UserDefaults.standard.removeObject(forKey: configPrefix + bundleID)
         notifySpringBoard()
     }
 
+    private static func findPlist(bundleID: String) throws -> URL {
+        let fm = FileManager.default
+        let bundleRoot = URL(fileURLWithPath: "/var/containers/Bundle/Application")
+
+        guard let uuids = try? fm.contentsOfDirectory(at: bundleRoot, includingPropertiesForKeys: nil) else {
+            throw AppDisguiseError.bundleNotFound(bundleID)
+        }
+
+        for uuid in uuids {
+            let apps = (try? fm.contentsOfDirectory(at: uuid, includingPropertiesForKeys: nil)) ?? []
+            for app in apps where app.pathExtension == "app" {
+                let plist = app.appendingPathComponent("Info.plist")
+                if let dict = NSDictionary(contentsOf: plist) as? [String: Any],
+                   let id = dict["CFBundleIdentifier"] as? String,
+                   id == bundleID {
+                    return plist
+                }
+            }
+        }
+
+        throw AppDisguiseError.bundleNotFound(bundleID)
+    }
+
     private static func notifySpringBoard() {
-        // Notify SpringBoard to refresh app icon cache
-        let notifyNames = [
-            "com.apple.mobile.application_installed",
-            "SBApplicationCacheChanged",
-        ]
-        for name in notifyNames {
+        for name in ["com.apple.mobile.application_installed", "SBApplicationCacheChanged"] {
             CFNotificationCenterPostNotification(
                 CFNotificationCenterGetDarwinNotifyCenter(),
                 CFNotificationName(name as CFString),
@@ -130,8 +132,8 @@ enum AppDisguiseService {
     private static func resize(image: UIImage, to size: CGSize) -> UIImage? {
         UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
         image.draw(in: CGRect(origin: .zero, size: size))
-        let resized = UIGraphicsGetImageFromCurrentImageContext()
+        let result = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        return resized
+        return result
     }
 }
